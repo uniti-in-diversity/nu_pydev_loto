4324c649e5f779b9d6ac5ca79511d442ecd2837d
maxim <dekin_m@mail.ru>
2019-11-30 02:09:12 +0300

    created: func create card, func show card

--- loto.py
+++ loto.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from collections import Counter
 
 def add_separators(f):
     def inner(*args, **kwargs):
@@ -26,99 +27,131 @@ def check_unic_nums(array):
     if i:
         return i
 
-def create_card():
-    '''
-    Генерация рандом массива 3 на 5 с заполнением случаными чилами от 1 до 90
-    Генерация нулевого массива 3 на 4
-    Конкатенация массивов по столбцам (axis = 1)
-    Переемешиваем элементы массива случайным образом в рамках каждой строки
-    :return: Массив 3 на 9, 5 элементов из 9 в каждой строке случайные числа,
-    остальные элементы 0. Тип элементов строка.
-    '''
-    nums = np.random.randint(1, 91, size=(15))
-    while check_unic_nums(nums):
+########################################################################################################
+########################################################################################################
+
+class Player:
+
+    def __init__(self, name, type):
+        self.computer = type
+        self.winer = False
+        self.name = name
+
+class Card():
+    def __init__(self):
+        '''
+        Генерация рандом массива 3 на 5 с заполнением случаными чилами от 1 до 90
+        Генерация нулевого массива 3 на 4
+        Конкатенация массивов по столбцам (axis = 1)
+        Переемешиваем элементы массива случайным образом в рамках каждой строки
+        :return: Массив 3 на 9, 5 элементов из 9 в каждой строке случайные числа,
+        остальные элементы 0. Тип элементов строка.
+        '''
         nums = np.random.randint(1, 91, size=(15))
-    print(nums)
-    nums = np.reshape(nums, (3, 5))
-    nums = nums.astype('str')
-    zero = np.array((['n', 'n', 'n', 'n'], ['n', 'n', 'n', 'n'], ['n', 'n', 'n', 'n']), str)
-    array = np.concatenate([nums, zero], axis = 1)
-    #далее перемешиваем в каждой строке элементы случайным образом
-    x, y = array.shape
-    rows = np.indices((x, y))[0]
-    cols = [np.random.permutation(y) for _ in range(x)]
-    #print(array[rows, cols])
-    return array[rows, cols]
-
-#Отобразить карточку
-@add_separators
-def showcard(card):
-    '''
-    Печатает массив в виде игровой карточки лото, нули заменяются пустыми строками (одиночными пробелами)
-    :param card: массив элементов
-    :return: Print в консоль
-    '''
-    for i in card:
-        lineforprint = (' '.join(map(str, i.ravel())))
-        print(lineforprint.replace('n', ' '))
+        while check_unic_nums(nums):
+            nums = np.random.randint(1, 91, size=(15))
+        #print(nums)
+        nums = np.reshape(nums, (3, 5))
+        nums = np.sort(nums)
+        nums = nums.astype('str')
+        zero = np.array((['n', 'n', 'n', 'n'], ['n', 'n', 'n', 'n'], ['n', 'n', 'n', 'n']), str)
+        array = np.concatenate([nums, zero], axis = 1)
+        x, y = array.shape
+        rows = np.indices((x, y))[0]
+        cols = [np.random.permutation(y) for _ in range(x)]
+        array = array[rows, cols]
+        self.array = array
 
 
-def bochonoknum_is_in_card(card, current_bochonok):
-    '''
-    Проверяет есть ли номер бочонка в карте
-    :param card: карта игрока (массив)
-    :param bochonok: номер бочонка пробразованный в строку
-    :return: индекс позиции номера бочонка в карте
-    '''
-    # print(np.unique(card_max == a, return_index=True))
-        #print(np.where(card_max == a))
-    if current_bochonok in card:
-        indexnum = np.where(card == current_bochonok)
-        return indexnum
+    @add_separators
+    def showcard(self):
+        '''
+        Печатает массив в виде игровой карточки лото, нули заменяются пустыми строками (одиночными пробелами)
+        :param card: массив элементов
+        :return: Print в консоль
+        '''
+        for i in self.array:
+            lineforprint = (' '.join(map(str, i.ravel())))
+            print(lineforprint.replace('n', ' '))
 
-def cover_bochonoknum(card, current_bochonok):
-    '''
-    Вычеркивает номер бочонка изкарты, если номер есть в карте.
-    Наличия номера бокочнка проверяется функцией "bochonoknum_is_in_card"
-    :param card: карта игрока
-    :param current_bochonok: номер вытянутого из мешка бочонка
-    :return: Карту с вычернкутым номером
-    '''
-    if current_bochonok in card:
-        indexnum = bochonoknum_is_in_card(card, current_bochonok)
-        card[indexnum] = '-'+current_bochonok+'-'
-    return card
+# def __iter__(self):
+#     return iter((self.__dict__.values()))
+#     #return iter(self.__dict__.items())
+
+    def bochonoknum_is_in_card(self, current_bochonok):
+        '''
+        Проверяет есть ли номер бочонка в карте
+        :param card: карта игрока (массив)
+        :param bochonok: номер бочонка пробразованный в строку
+        :return: индекс позиции номера бочонка в карте
+        '''
+        # print(np.unique(card_max == a, return_index=True))
+            #print(np.where(card_max == a))
+        if current_bochonok in self.array:
+            indexnum = np.where(self.array == current_bochonok)
+            return indexnum
+
+    def cover_bochonoknum(self, current_bochonok):
+        '''
+        Вычеркивает номер бочонка изкарты, если номер есть в карте.
+        Наличия номера бокочнка проверяется функцией "bochonoknum_is_in_card"
+        :param card: карта игрока
+        :param current_bochonok: номер вытянутого из мешка бочонка
+        :return: Карту с вычернкутым номером
+        '''
+        if current_bochonok in self.array:
+            # indexnum = Card.bochonoknum_is_in_card(card, current_bochonok)
+            indexnum = self.bochonoknum_is_in_card(current_bochonok)
+            #print(indexnum)
+            self.array[indexnum] = '-' + '=' + '-'
+        return self.array
+
+    def check_win(self):
+        '''
+        Проверка карты, все ли зачеркнуты номера,
+        :param card: карта игрока для проверки
+        :return: True если все номера зачеркнуты, т.е. n = 27
+        '''
+        templist = []
+        for i in self.array:
+            for n in i:
+                templist.append(n)
+        if Counter(templist).get('n') == 27:
+            return True
+        else:
+            return False
 
 
 #ПРОЦЕДУРА ИГРЫ
 #Объявляем мешок как константу, всегда 90 шт.
 BOCHONKI = [i for i in range(1, 91)]
 #print(BOCHONKI)
-
-#Создаем объект карту
-card_max = create_card()
-#Печатает карту для ознакомления
-showcard(card_max)
-#print(type(card_max))
-
+players = []
 #Вытаскиваем случаный бочонок
 current_bochonok = str(BOCHONKI.pop(random.randrange(len(BOCHONKI))))
 print('Текущий номер бочонка', current_bochonok)
 #print(BOCHONKI)
 
+maxim_card = Card()
+#print(maxim_card)
+maxim_card.showcard()
+
+if maxim_card.check_win():
+    print('победитель')
+else:
+    print('еще не все зачеркнули')
+
 #проверяем цифру в карте
-if bochonoknum_is_in_card(card_max, current_bochonok):
+if Card.bochonoknum_is_in_card(maxim_card, current_bochonok):
     print('есть в карте')
 
+
 #зачеркиваем цифру
-card_max = cover_bochonoknum(card_max, current_bochonok)
-showcard(card_max)
+#cover_bochonoknum(maxim_card., current_bochonok)
+maxim_card.cover_bochonoknum(current_bochonok)
+maxim_card.showcard()
 
 #после каждого хода проверяем победителя
-#если в массиве количество n = 27
-
-
 
 #игра окончена, объявляем победителя
-
 
