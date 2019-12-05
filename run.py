6623a5f794a0b27967696480a1a34c8eb66216b4
maxim <dekin_m@mail.ru>
2019-12-06 00:28:08 +0300

    release

--- run.py
+++ run.py
@@ -1,16 +1,30 @@
 import game
+import loto
 
-while True:
-    print('========== Лото ============')
-    print('Новая игра - 1')
-    print('Выход - 0')
+if __name__ == '__main__':
 
-    choice = int(input('Выберите пункт меню: '))
-    if choice == 1:
-        count_players = int(input('Введите количество игроков: '))
-        start = game.Game()
-        start.run(count_players)
-    elif choice == 0:
-        break
-    else:
-        print('Неверный пункт меню!')
+    while True:
+        print('========== Лото ============')
+        print('Новая игра - 1')
+        print('Выход - 0')
+
+        choice = int(input('Выберите пункт меню: '))
+        if choice == 1:
+            int_players = []
+            count_players = int(input('Введите количество игроков: '))
+            for i in range(count_players):
+                player_name = input('Введите имя игрока')
+                player_type = int(input('Введите тип игрока, Если человек - введите 1, если компьютер - введите 0:'))
+                if player_type:
+                    player = loto.Player(player_name, True, False)
+                    int_players.append(player)
+                else:
+                    player = loto.Player(player_name, False, False)
+                    int_players.append(player)
+
+            start = game.Game()
+            start.run(int_players)
+        elif choice == 0:
+            break
+        else:
+            print('Неверный пункт меню!')
---
+++ test.py
@@ -0,0 +1,104 @@
+import loto
+import game
+import unittest
+
+class TestPlayer(unittest.TestCase):
+
+    def testPlayer(self):
+        self.player = loto.Player('Testplayer', True, False)
+        self.assertEquals(self.player.winer, False)
+        self.assertIsInstance(self.player, loto.Player)
+
+
+class TestCard(unittest.TestCase):
+
+    def setUp(self):
+        self.testcard = loto.Card('card_for_test')
+
+    def test_init_card(self):
+        self.assertEqual(self.testcard.username, 'card_for_test')
+
+    def test_card(self):
+        trigger = []
+        for num in self.testcard.array:
+            for i in num:
+                if i == 'n':
+                    trigger.append(i)
+        self.assertEquals(len(trigger), 12)
+
+    def test_is_num_in_card(self):
+        self.assertIs(loto.Card.bochonoknum_is_in_card(self.testcard, '0'), None)
+
+    def test_cover_bochonok(self):
+        nums = []
+        newnums = []
+        for n in self.testcard.array:
+            for a in n:
+                nums.append(a)
+        checkednum = nums[0]
+        newcard = self.testcard.cover_bochonoknum(checkednum)
+        for num in newcard:
+            for i in num:
+                newnums.append(i)
+        self.assertNotEqual(nums, newnums)
+
+    def test_win(self):
+        self.assertIs(self.testcard.check_win(), False)
+
+class TetsBochonki(unittest.TestCase):
+
+    def setUp(self):
+        self.testbag = loto.Bag()
+
+    def test_bochonki(self):
+        self.assertEqual(self.testbag.__class__.__name__, 'Bag')
+
+    def test_take_num(self):
+        assert 0 < self.testbag.take_bochonok() < 90
+
+class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.testgame = game.Game()
+        self.int_players = []
+        self.winers = []
+        self.player1 = loto.Player('comp1', False, False)
+        self.player2 = loto.Player('comp2', False, False)
+        self.int_players.append(self.player1)
+        self.int_players.append(self.player2)
+        self.lottobag = loto.Bag()
+
+    def get_input(text):
+        return input(text)
+
+    def test_game_init(self):
+        self.assertIsInstance(self.testgame, game.Game)
+
+    def test_gaming(self):
+        self.testgame.run(self.int_players)
+        self.assertIsNotNone(self.testgame.winers)
+        self.assertIs(self.testgame.usererror, False)
+
+class TestGameHumans(unittest.TestCase):
+
+    def setUp(self):
+        self.testgame = game.Game()
+        self.int_players = []
+        self.winers = []
+        self.player1 = loto.Player('comp1', True, False)
+        self.player2 = loto.Player('comp2', True, False)
+        self.int_players.append(self.player1)
+        self.int_players.append(self.player2)
+        self.lottobag = loto.Bag()
+
+    def get_input(text):
+        return input(text)
+
+    def test_gamin_human(self):
+        self.testgame.run(self.int_players)
+        self.assertIs(self.testgame.usererror, True)
+
+
+
+
+
